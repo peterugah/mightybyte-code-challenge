36a7fd0199b60437df7e36fcc06594ef10d22f77
peterugah <p.ugah.pu@gmail.com>
2025-05-23 00:11:30 +0100

    feat: readme

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,19 +1,46 @@
-import { DriverLoginDto } from '@monorepo/shared';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { DriverLoginDto, RefreshDriverTokenDto } from '@monorepo/shared';
+import { BadRequestException, Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { DriverService } from 'src/driver/driver.service';
-import { driverLoginValidatorSchema } from 'src/driver/driver.validator';
+import { driverLoginValidatorSchema, refreshTokenValidatorSchema } from 'src/driver/driver.validator';
 import { TokenGuard } from 'src/jwt/jwt.guard';
+import { JwtService } from 'src/jwt/jwt.service';
 import { ZodValidationPipe } from 'src/utils/zod.pipe';
 
 @Controller('')
 @UseGuards(TokenGuard)
 export class AppController {
-  constructor(private readonly driverService: DriverService) { }
+  constructor(private readonly driverService: DriverService,
+    private readonly jwtService: JwtService,
+  ) { }
+
   @Post('login')
   login(
     @Body(new ZodValidationPipe(driverLoginValidatorSchema))
     payload: DriverLoginDto,
   ) {
     return this.driverService.login(payload);
+  }
+  /**
+  INFO: 
+    When the driver's token expires, he gets a token expired error with a 600 statusCode for both an HTTP and WS connection. This endpoint is used to refresh the token which can then be used to make the request again and subsequent requests.
+*/
+  @Post('refresh-token')
+  async refreshToken(
+    @Body(new ZodValidationPipe(refreshTokenValidatorSchema))
+    payload: RefreshDriverTokenDto,
+  ) {
+    await this.jwtService.verifyRefreshToken(payload.refreshToken);
+    const driver = await this.driverService.getDetailsByRefreshToken(
+      payload.refreshToken,
+    );
+    if (!driver) {
+      throw new BadRequestException(`Unabled to refresh drivers's token`);
+    }
+
+    const { token, refreshToken } = this.driverService.generateTokens(
+      driver.id,
+    );
+    await this.driverService.updateRefreshToken(refreshToken, driver.id);
+    return { token, refreshToken, driver };
   }
 }
--- apps/backend/src/driver/driver.controller.ts
+++ apps/backend/src/driver/driver.controller.ts
@@ -3,21 +3,18 @@ import {
   Controller,
   Post,
   UseGuards,
-  BadRequestException,
   Inject,
   Get,
 } from '@nestjs/common';
 import { TokenPayload, ValidateToken } from 'src/jwt/jwt.decorator';
 import {
-  refreshTokenValidatorSchema,
   updateLocationValidatorSchema,
 } from './driver.validator';
-import { UpdateDriveLocationDto, RefreshDriverTokenDto } from '@monorepo/shared';
+import { UpdateDriveLocationDto, } from '@monorepo/shared';
 import { TokenDto } from 'src/jwt/jwt.type';
 import { DriverService } from './driver.service';
 import { ZodValidationPipe } from 'src/utils/zod.pipe';
 import { TokenGuard } from 'src/jwt/jwt.guard';
-import { JwtService } from 'src/jwt/jwt.service';
 import { BrokerServices } from 'src/broker/broker.enum';
 import { ClientProxy } from '@nestjs/microservices';
 import { DriverEvents } from './driver.enum';
@@ -28,7 +25,6 @@ import { DriverLocationDetails } from './driver.type';
 export class DriverController {
   constructor(
     private readonly driverService: DriverService,
-    private readonly jwtService: JwtService,
     @Inject(BrokerServices.DRIVER_SERVICE)
     private readonly driverClient: ClientProxy,
   ) { }
@@ -54,27 +50,4 @@ export class DriverController {
     return location;
   }
 
-  /**
-  INFO: 
-    When the driver's token expires, he gets a token expired error with a 600 statusCode for both an HTTP and WS connection. This endpoint is used to refresh the token which can then be used to make the request again and subsequent requests.
- */
-  @Post('refresh-token')
-  async refreshToken(
-    @Body(new ZodValidationPipe(refreshTokenValidatorSchema))
-    payload: RefreshDriverTokenDto,
-  ) {
-    await this.jwtService.verifyRefreshToken(payload.refreshToken);
-    const driver = await this.driverService.getDetailsByRefreshToken(
-      payload.refreshToken,
-    );
-    if (!driver) {
-      throw new BadRequestException(`Unabled to refresh drivers's token`);
-    }
-
-    const { token, refreshToken } = this.driverService.generateTokens(
-      driver.id,
-    );
-    await this.driverService.updateRefreshToken(refreshToken, driver.id);
-    return { token, refreshToken, driver };
-  }
 }
